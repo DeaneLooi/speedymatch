e46432ee9f179a79fa50a2e0921f7d7db687233c
Deane <123188g@mymail.nyp.edu.sg>
2014-01-07 12:22:45 +0800

    added entities

---
+++ speedymatch/src/speedymatch/entities/Member.java
@@ -0,0 +1,93 @@
+package speedymatch.entities;
+
+import java.sql.Date;
+
+public class Member {
+
+	private String username;
+
+	private String passwd;
+
+	private String email;
+
+	private String Fname;
+
+	private String Lname;
+
+	private Date dob;
+	
+	private MemberSecurity memberSecurity;
+	
+	public Member(String username, String passwd){
+		
+		this.username = username;
+		this.passwd = passwd;
+	}
+
+	public Member(String username, String passwd, String email, String Fname, String Lname, Date dob){
+		
+		this.username = username;
+		this.passwd = passwd;
+		this.email = email;
+		this.Fname = Fname;
+		this.Lname = Lname;
+		this.dob = dob;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFname() {
+		return Fname;
+	}
+
+	public void setFname(String fname) {
+		Fname = fname;
+	}
+
+	public String getLname() {
+		return Lname;
+	}
+
+	public void setLname(String lname) {
+		Lname = lname;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	
+	public MemberSecurity getMemberSecurity(){
+		return memberSecurity;
+	}
+	
+	public void addMemberSecurity(MemberSecurity memberSecurity){
+		this.memberSecurity = memberSecurity;
+	}
+
+}
---
+++ speedymatch/src/speedymatch/entities/MemberSecurity.java
@@ -0,0 +1,78 @@
+package speedymatch.entities;
+
+import java.sql.Date;
+
+public class MemberSecurity {
+	
+	private String salt;
+	
+	private String token;
+	
+	private String membership;
+	
+	private Date regDate;
+	
+	private char disabled;
+	
+	private String communicaton;
+
+	
+	public MemberSecurity(String salt, String token, String membership, Date regDate, char disabled){
+		
+		this.salt = salt;
+		this.token = token;
+		this.membership = membership;
+		this.regDate = regDate;
+		this.disabled = disabled;
+		
+	}
+	
+	public String getSalt() {
+		return salt;
+	}
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public String getMembership() {
+		return membership;
+	}
+
+	public void setMembership(String membership) {
+		this.membership = membership;
+	}
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+
+	public char getDisabled() {
+		return disabled;
+	}
+
+	public void setDisabled(char disabled) {
+		this.disabled = disabled;
+	}
+	
+	public String getCommunication(){
+		return communicaton;
+	}
+	
+	public void setCommunication(String communication){
+		this.communicaton = communication;
+	}
+
+}
