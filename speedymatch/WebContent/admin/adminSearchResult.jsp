8782088a8f2c3aa8341dd757c2188d79dd4a8533
yuwen <yw@yw-PC>
2014-01-28 17:29:37 +0800

    search

--- speedymatch/WebContent/admin/adminSearchResult.jsp
+++ speedymatch/WebContent/admin/adminSearchResult.jsp
@@ -1,9 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
 
-<%@ page import="java.sql.*" %>
-
-<% Class.forName("com.mysql.jdbc.Driver"); %>
-
+<%@ page import="java.util.*" %>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
 
@@ -18,20 +15,53 @@
 <%@ include file="adminHeader.jsp" %>
 <center>
 <!-- content here -->
-<div id="tfheader">
-		
-		<ul>
-<li><p><b>Username:</b>
-   <%= request.getParameter("adminSearch")%>
-</p></li>
-		
-	</div>
-	
-	<!--temp div-->
-	<div class="logo-background"> 
-   
-            
-	</div>
+
+<table width="1000" align="center" style="border:1px solid #000000;">
+            <tr>
+                <td colspan=4 align="center"
+                    style="background-color:#F13C73;">
+                    <b>Result</b></td>
+            </tr>
+            <tr style="background-color:white;">
+                <td><b>User id</b></td>
+                <td><b>Name</b></td>
+                <td><b>Gender</b></td>
+                <!-- <td><b>Registration Date</b></td> -->
+            </tr>
+            <%
+                int count = 0;
+                String color = "white";
+                if (request.getAttribute("piList") != null) {
+                    ArrayList al = (ArrayList) request.getAttribute("piList");
+                    System.out.println(al);
+                    Iterator itr = al.iterator();
+                    while (itr.hasNext()) {
+ 
+                        if ((count % 2) == 0) {
+                            color = "white";
+                        }
+                        count++;
+                        ArrayList pList = (ArrayList) itr.next();
+            %>
+            <tr style="background-color:<%=color%>;">
+                <td><%=pList.get(0)%></td>
+                <td><%=pList.get(1)%></td>
+                <td><%=pList.get(2)%></td>
+               <%--  <td><%=pList.get(3)%></td> --%>
+            </tr>
+            <%
+                    }
+                }
+                if (count == 0) {
+            %>
+            <tr>
+                <td colspan=4 align="center"
+                    style="background-color:white"><b>No Record Found..</b></td>
+            </tr>
+            <%            }
+            %>
+        </table>
+
 </center>
 <%@ include file="../pages/footer.jsp" %>
 </body>
--- speedymatch/src/speedymatch/servlets/AdminSearch.java
+++ speedymatch/src/speedymatch/servlets/AdminSearch.java
@@ -2,16 +2,19 @@ package speedymatch.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class AdminSearch
- */
 @WebServlet("/AdminSearch")
 public class AdminSearch extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -29,32 +32,63 @@ public class AdminSearch extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.setContentType("text/html");
-
-	      PrintWriter out = response.getWriter();
-		  String title = "Using GET Method to Read Form Data";
-	      String docType =
-	      "<!doctype html public \"-//w3c//dtd html 4.0 " +
-	      "transitional//en\">\n";
-	      out.println(docType +
-	                "<html>\n" +
-	                "<head><title>" + title + "</title></head>\n" +
-	                "<body bgcolor=\"#f0f0f0\">\n" +
-	                "<h1 align=\"center\">" + title + "</h1>\n" +
-	                "<ul>\n" +
-	                "  <li><b>First Name</b>: "
-	                + request.getParameter("first_name") + "\n" +
-	                "  <li><b>Last Name</b>: "
-	                + request.getParameter("last_name") + "\n" +
-	                "</ul>\n" +
-	                "</body></html>");
+	         
 	}
+	
+	public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        Connection conn = null;
+        String url = "jdbc:mysql://127.0.0.1:3306/";
+        String dbName = "speedymatch";
+        String driver = "com.mysql.jdbc.Driver";
+        String userName = "adming1vWayv";
+        String password = "2MVCBKUIFISG";
+        
+        Statement st;
+        try {
+            Class.forName(driver).newInstance();
+            conn = DriverManager.getConnection(url + dbName, userName, password);
+            System.out.println("Connected!");
+            String username = request.getParameter("username");
+ 
+            ArrayList al = null;
+            ArrayList pid_list = new ArrayList();
+            String query = "select * from Profiles where username='" + username + "' ";
+ 
+            System.out.println("query " + query);
+            st = conn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+ 
+            while (rs.next()) {
+                al = new ArrayList();
+ 
+                al.add(rs.getString(1));
+                al.add(rs.getString(2));
+                al.add(rs.getString(3));
+               // al.add(rs.getString(4));
+ 
+                System.out.println("al :: " + al);
+                pid_list.add(al);
+                
+            }
+            
+            request.setAttribute("piList", pid_list);
+            RequestDispatcher view = request.getRequestDispatcher("admin/adminSearchResult.jsp");
+            view.forward(request, response);
+            conn.close();
+            System.out.println("Disconnected!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 	}
 
 }
