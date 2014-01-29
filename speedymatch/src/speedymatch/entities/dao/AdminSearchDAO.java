af84558f1594df58e348b6ae3f51774f617e7164
yuwen <yw@yw-PC>
2014-01-29 17:07:35 +0800

    adminDAO

--- speedymatch/src/speedymatch/entities/dao/AdminSearchDAO.java
+++ speedymatch/src/speedymatch/entities/dao/AdminSearchDAO.java
@@ -7,38 +7,6 @@ import speedymatch.entities.Member;
 import speedymatch.entities.Profile;
 
 public class AdminSearchDAO{
-	/*public boolean adminSearch(){
-		DBManager db = new DBManager();
-		Connection currentCon = db.getConnection();
-		ResultSet r =null;
-		Member m = null;
-		
-		try{
-            String query="select * from Profiles"; 
-            
-            while(r.next()){
-                    r.getString("username");
-                    r.getString("gender");
-                    r.getString("age");
-            }
-
-    }catch(Exception ex){
-            ex.printStackTrace();
-            m = null;
-    }finally {
-
-            if (currentCon != null) {
-                    try {
-                            currentCon.close();
-                    } catch (Exception e) {
-                    }
-
-                    currentCon = null;
-            }
-    }
-		return false;
-
-  }*/
 		
 	public boolean adminSearch(){
 		boolean success = false;
@@ -71,25 +39,4 @@ public class AdminSearchDAO{
 		a.adminSearch();
 	}
 	
-	
-	
-	/*public static void main(String[] args) throws Exception{
-	      Class.forName("com.mysql.jdbc.Driver");
-	      Connection con = DriverManager.getConnection
-	      ("jdbc:mysql://127.0.0.1:3306/speedymatch","adming1vWayv",
-	      "2MVCBKUIFISG");
-	      Statement stmt = con.createStatement();
-	      String query[] ={"SELECT * FROM Profiles"};
-	      for(String q : query){
-	         ResultSet rs = stmt.executeQuery(q);
-	         while (rs.next()) {
-	            int username = rs.getInt("username");
-	            String age = rs.getString("age");
-	            String gender = rs.getString("gender");
-	            System.out.print(username+"  "+age+" "+gender+" ");
-	            
-	         }
-	         System.out.println();
-	      }
-	   }*/
 }
