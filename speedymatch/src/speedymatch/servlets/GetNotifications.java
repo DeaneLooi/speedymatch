72ec0023cdac41cb35e0bf21d0bee267da3e04fa
tanwk547 <Tan Wai Kit@tnwaikit@hotmail.com>
2014-02-08 12:54:11 +0800

    Added friend and message notification

--- speedymatch/src/speedymatch/servlets/GetNotifications.java
+++ speedymatch/src/speedymatch/servlets/GetNotifications.java
@@ -21,85 +21,114 @@ import speedymatch.utils.NotificationVariables;
 @WebServlet("/GetNotifications")
 public class GetNotifications extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public GetNotifications() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public GetNotifications() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+
 		response.setContentType("text/xml");
-	    response.setHeader("Cache-Control", "no-cache");
-	    
+		response.setHeader("Cache-Control", "no-cache");
+
 		Member m = (Member) request.getSession().getAttribute("member");
-		Notification n = new Notification(null,m.getUsername(),null);
-		ArrayList<Notification> notifications = NotificationDAO.retrieveNotifications(n);
-		
-		for (int i=0; i<notifications.size(); i++){
-		System.out.println("retrieved notification = "+notifications.get(i).getSender());
-		System.out.println(notifications.get(i).getReceiver());
-		System.out.println(notifications.get(i).getNotification());
+		Notification n = new Notification(null, m.getUsername(), null);
+		ArrayList<Notification> notifications = NotificationDAO
+				.retrieveNotifications(n);
+
+		for (int i = 0; i < notifications.size(); i++) {
+			System.out.println("retrieved notification = "
+					+ notifications.get(i).getSender());
+			System.out.println(notifications.get(i).getReceiver());
+			System.out.println(notifications.get(i).getNotification());
 		}
 		PrintWriter writer = response.getWriter();
-		
-		String html = "";		
-		
-		for(int i=0; i<notifications.size();i++){
+
+		String html = "";
+
+		for (int i = 0; i < notifications.size(); i++) {
 			Notification n1 = notifications.get(i);
-			
-			if(n1.getNotification().equals(NotificationVariables.ALERT)){
-				html+="<div style='background-color:lightgray;border-style:solid;border-color:white;border-width:1px;padding:5px'>";
-				html+="<p>"+n1.getSender()+" has visited your profile&nbsp;<a href='../DeleteNotification?content=Alert'>&#x2717</a></p>";
-				html+="</div>";
+
+			if (n1.getNotification().equals(NotificationVariables.ALERT)) {
+				html += "<div style='background-color:lightgray;border-style:solid;border-color:white;border-width:1px;padding:5px'>";
+				html += "<p>"
+						+ n1.getSender()
+						+ " has visited your profile&nbsp;<a href='../DeleteNotification?content=Alert'>&#x2717</a></p>";
+				html += "</div>";
 			}
-			
-			else if(n1.getNotification().equals(NotificationVariables.CINEMA)){
-				html+="<div style='background-color:lightgray;border-style:solid;border-color:white;border-width:1px;padding:5px'>";
-				html+="<p>"+n1.getSender()+" has invited you to watch a movie</p>";
-				html+="<a href='../Cinema'>Accept</a>&nbsp;";
-				html+="<a href='../DeleteNotification?content=Cinema'>Reject</a>";
-				html+="</div>";
-				
+
+			else if (n1.getNotification().equals(NotificationVariables.CINEMA)) {
+				html += "<div style='background-color:lightgray;border-style:solid;border-color:white;border-width:1px;padding:5px'>";
+				html += "<p>" + n1.getSender()
+						+ " has invited you to watch a movie</p>";
+				html += "<a href='../Cinema'>Accept</a>&nbsp;";
+				html += "<a href='../DeleteNotification?content=Cinema'>Reject</a>";
+				html += "</div>";
+
 			}
-			
-			else if(n1.getNotification().equals(NotificationVariables.FRIEND)){
-				
-				html+="<p>"+n1.getSender()+" has requested to be your friend</p>";
+
+			else if (n1.getNotification().equals(NotificationVariables.FRIEND)) {
+				System.out.println("add friendNotification has appear");
+				html += "<div style='background-color:lightgray;border-style:solid;border-color:white;border-width:1px;padding:5px'>";
+				html += "<p>" + n1.getSender()
+						+ " has requested to be your friend</p>";
+				html += "<a href='../AddFriend'>Accept</a>&nbsp;";
+				html += "<a href='../DeleteNotification?content="
+						+ NotificationVariables.FRIEND + "&sender="
+						+ n1.getSender() + "&receiver=" + n1.getReceiver()
+						+ "'>Reject</a>";
+				html += "</div>";
 			}
-			
-			else if(n1.getNotification().equals(NotificationVariables.VIDEOCONF)){
+
+			else if (n1.getNotification().equals(
+					NotificationVariables.VIDEOCONF)) {
 				System.out.println("Showing vidconf notofication.");
-				html+="<div style='background-color:lightgray;border-style:solid;border-color:white;border-width:1px;padding:5px'>";
-				html+="<p>"+n1.getSender()+" has invited you to a video chat</p>";
-				html+="<a href='../'>Accept</a>&nbsp;";
-				html+="<a href='../DeleteNotification?content="+NotificationVariables.VIDEOCONF+"&sender="+n1.getSender()+"&receiver="+n1.getReceiver()+"'>Reject</a>";
-				html+="</div>";		
+				html += "<div style='background-color:lightgray;border-style:solid;border-color:white;border-width:1px;padding:5px'>";
+				html += "<p>" + n1.getSender()
+						+ " has invited you to a video chat</p>";
+				html += "<a href='../'>Accept</a>&nbsp;";
+				html += "<a href='../DeleteNotification?content="
+						+ NotificationVariables.VIDEOCONF + "&sender="
+						+ n1.getSender() + "&receiver=" + n1.getReceiver()
+						+ "'>Reject</a>";
+				html += "</div>";
 			}
-			
-			else if(n1.getNotification().equals(NotificationVariables.MESSAGE)){
-				html+="<p>"+n1.getSender()+" just messaged you</p>";
+
+			else if (n1.getNotification().equals(NotificationVariables.MESSAGE)) {
+				System.out.println("add messageNotification has appear");
+				html += "<div style='background-color:lightgray;border-style:solid;border-color:white;border-width:1px;padding:5px'>";
+				html += "<p>" + n1.getSender() + " just messaged you</p>";
+				html += "<a href='../AddFriend'>Accept</a>&nbsp;";
+				html += "<a href='../DeleteNotification?content="
+						+ NotificationVariables.MESSAGE + "&sender="
+						+ n1.getSender() + "&receiver=" + n1.getReceiver()
+						+ "'>Reject</a>";
+				html += "</div>";
 			}
-			
+
 		}
-		
-		
+
 		writer.println(html);
-		
+
 		writer.close();
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
