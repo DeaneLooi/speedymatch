dd926d6da6fb52a0919f6074ba767a6bf12f0d0f
yuwen <yw@yw-PC>
2014-02-10 03:45:18 +0800

    working dao

--- speedymatch/src/speedymatch/entities/dao/AdminSearchDAO.java
+++ speedymatch/src/speedymatch/entities/dao/AdminSearchDAO.java
@@ -1,88 +1,74 @@
 package speedymatch.entities.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
+import java.util.*;
+import java.sql.*;
 
-import speedymatch.entities.FriendList;
-import speedymatch.entities.Member;
-import speedymatch.entities.MemberSecurity;
 import speedymatch.entities.Profile;
-import speedymatch.utils.Algorithms;
 
-public class AdminSearchDAO {
-
-	private static DBController db = new DBController();
-
-	public static Member advanceSearch(Member profile) {
-		Connection currentCon = db.getConnection();
-		Member m = null;
-		ResultSet rs = null;
-
-		try {
-			ArrayList<String> al = null;
-			ArrayList pid_list = new ArrayList();
-			
-			String [] query={"select m.username from Member m inner join Profile p "
-					+ " m.username=p.username where p.gender='m'", "select m.username from Member m inner join Profile p on m.username=p.username where p.smoking='no'", "select m.username from Member m inner join Profile p on m.username=p.username where p.drinking='no'", "select m.username from Member m inner join Profile p on m.username=p.username where p.relaStatus='single'", "select m.username from Member m inner join Profile p on m.username=p.username where p.child='no'", "select m.username from Member m inner join Profile p on m.username=p.username where p.eduLevel='master'", "select m.username from Member m inner join Profile p on m.username=p.username where p.horo='pisces'", "select m.username from Member m inner join Profile p on m.username=p.username where p.occupation='business owner'", "select m.username from Member m inner join Profile p on m.username=p.username where p.race='Malay'", "select m.username from Member m inner join Profile p on m.username=p.username where p.religion='Buddhist'", "select m.username from Member m inner join Profile p on m.username=p.username where p.hobby='gardening'"};
-			/*String query1="select m.username from Member m inner join Profile p on m.username=p.username where p.gender='male'";
-			String query2="select m.username from Member m inner join Profile p on m.username=p.username where p.smoking='no'";
-			String query3="select m.username from Member m inner join Profile p on m.username=p.username where p.drinking='no'";
-			String query4="select m.username from Member m inner join Profile p on m.username=p.username where p.relaStatus='single'";
-			String query5="select m.username from Member m inner join Profile p on m.username=p.username where p.child='no'";
-			String query6="select m.username from Member m inner join Profile p on m.username=p.username where p.education='master'";
-			String query7="select m.username from Member m inner join Profile p on m.username=p.username where p.horo='pisces'";
-			String query8="select m.username from Member m inner join Profile p on m.username=p.username where p.occupation='business owner'";
-			String query9="select m.username from Member m inner join Profile p on m.username=p.username where p.race='Malay'";
-			String query10="select m.username from Member m inner join Profile p on m.username=p.username where p.religion='Buddhist'";
-			String query11="select m.username from Member m inner join Profile p on m.username=p.username where p.hobby='gardening'";
-			String query = query1 + query2 + query3 + query4 + query5 +query6 + query7 + query8 + query9 + query10 + query11;*/
-			//String query = "select username, gender,smoking, drinking, relaStatus, child, eduLevel, horo, occupation, race, religion, hobby from Profile where username='deane'";
-			//String query = "select m.username, p.gender, p.smoking, p.drinking, p.relaStatus, p.child, p.eduLevel, p.horo, p.occupation, p.race, p.religion, p.hobby from Member m inner join Profile";
-			//PreparedStatement pstmt = currentCon.prepareStatement(query);
-			//rs = pstmt.executeQuery();
-			
-			for (int i=0; i<11; i++){
-				PreparedStatement pstmt = currentCon.prepareStatement(query[i]);
-			
-			rs=pstmt.executeQuery(query[i]);
-			pstmt.close();
+public class AdminSearchDAO{
+	
+	static Connection currentcon = null;
+	static ResultSet rs = null;
+	static PreparedStatement pt = null;
+	
+	//can retrieve
+	public boolean adminSearch(){
+		boolean success = false;
+		ResultSet r =null;
+		DBManager db = new DBManager();
+		Connection con=DBManager.getConnection();
+		
+		
+		String dbQuery = "select * from Member m inner join Profile p on m.username = p.username where p.hobby = 'collecting'";
+		//String dbQuery = "select m.username, p.age, p.gender, p.eduLevel, p.hobby, p.height, p.weight, p.race, p.religion, p.child, p.horo, p.occupation, p.smoking, p.drinking, p.relaStatus from Member m inner join Profile p on m.username = p.username where p.hobby='gardening'";
+				//='" + hobby +"'";
+		r=db.readRequest(dbQuery);
+		
+		try{
+			if (r.next() ){
+				String username=r.getString("username");
+				int age=r.getInt("age");
+				String gender=r.getString("gender");
+				String eduLevel = r.getString("eduLevel");
+				String hobby = r.getString("hobby");
+				int weight = r.getInt("weight");
+				int height = r.getInt("height");
+				String race = r.getString("race");
+				String religion = r.getString("religion");
+				String child = r.getString("child");
+				String horo = r.getString("horo");
+				String occupation = r.getString("occupation");
+				String smoking = r.getString("smoking");
+				String drinking = r.getString("drinking");
+				String relaStatus = r.getString("relaStatus");
+				
+				System.out.println(username+" "+age+" "+gender+" "+ eduLevel +" "+hobby+" "+weight+" "+height+" "+race+" "+religion+" "+child+" "+horo+" "+occupation+" "+smoking+" "+drinking+" "+relaStatus+" ");
+				
+				System.out.println(username);
+				success=true;
+				
 			}
-			while (rs.next()) {
-
-				al = new ArrayList();
-
-				al.add(rs.getString(1));
-				al.add(rs.getString(2));
-				al.add(rs.getString(3));
-				al.add(rs.getString(4));
-				al.add(rs.getString(5));
-				al.add(rs.getString(6));
-				al.add(rs.getString(7));
-				al.add(rs.getString(8));
-				al.add(rs.getString(9));
-				al.add(rs.getString(10));
-				al.add(rs.getString(11));
-
-				System.out.println("al :: " + al);
-				pid_list.add(al);
-
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		} finally {
-
-			if (currentCon != null) {
-				try {
-					currentCon.close();
-				} catch (Exception e) {
-				}
-
-				currentCon = null;
-			}
+		}catch(Exception e){
+			e.printStackTrace();
+			
 		}
-		return profile;
+	db.terminate();
+	return success;
 	}
-
+	
+	
+	//mian method
+	public static void main(String[] args){
+		AdminSearchDAO a = new AdminSearchDAO();
+		//a.retrieveList();
+		a.adminSearch();
+//		ArrayList <Profile> searchList = new ArrayList<Profile>();
+//		for(int i=0; i< searchList.size(); i++){
+//			
+//			p = searchList.get(i);
+//			System.out.println("item" + i + ":" + p);
+//		}	
+		
+	}
+	
 }
